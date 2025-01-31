cb834bb7ca2d33e0362381d0c064a8ba221f7510
Madhavan <madhaone0@gmail.com>
2025-01-31 13:21:39 +0530

    commit1

---
+++ src/main/java/com/example/demo_portpolio_website/DemoPortpolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_portpolio_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPortpolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPortpolioWebsiteApplication.class, args);
+	}
+
+}
